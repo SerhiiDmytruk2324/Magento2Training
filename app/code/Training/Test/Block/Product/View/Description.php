7669840b042f0baf773f20c69c01985ff18ce8b8
Serhii Dmytruk <dmytruk.serhii@gmail.com>
2021-06-20 19:46:21 +0300

    3.4,3.5 not done

---
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Training\Test\Block\Product\View;
+class Description extends \Magento\Catalog\Block\Product\View\Description
+{
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+        $subject->getProduct()->setDescription('Test description');
+    }
+}
