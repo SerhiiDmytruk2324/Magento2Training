9d0e59f9eaca436b4cc19db00f76241e88cfcbe5
Serhii Dmytruk <dmytruk.serhii@gmail.com>
2021-06-20 04:06:43 +0300

    2.1 not done

--- app/code/Training/TestOM/Controller/Index/Index.php
+++ app/code/Training/TestOM/Controller/Index/Index.php
@@ -3,10 +3,11 @@
 namespace Training\TestOM\Controller\Index;
 
 use \Magento\Framework\App\Action\HttpGetActionInterface;
+use \Magento\Framework\App\Action\HttpPostActionInterface;
 use \Training\TestOM\Model\PlayWithTest;
 use \Training\TestOM\Model\Test;
 
-class Index implements HttpGetActionInterface
+class Index implements HttpGetActionInterface, HttpPostActionInterface
 {
     private $test;
     private $playtest;
