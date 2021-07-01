071573a1f123d5b999f3e5e0a4617796d435d112
Serhii Dmytruk <dmytruk.serhii@gmail.com>
2021-07-01 05:29:34 +0300

    3.3 not done

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -3,31 +3,41 @@
 namespace Training\Test\Controller\Block;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\Action\HttpPostActionInterface;
 
-class Index extends \Magento\Framework\App\Action\Action implements HttpGetActionInterface, HttpPostActionInterface
+class Index extends \Magento\Framework\App\Action\Action implements HttpGetActionInterface
 {
+
     /**
      * @var \Magento\Framework\View\LayoutFactory
      */
     private $layoutFactory;
 
     /**
+     * @var \Magento\Framework\Controller\Result\RawFactory
+     */
+    private $resultRawFactory;
+
+    /**
      * Index constructor.
      * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Framework\View\LayoutFactory $layoutFactory
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
         \Magento\Framework\View\LayoutFactory $layoutFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
         parent::__construct($context);
     }
     public function execute()
     {
-        $layout = $this->layoutFactory->create();
-        $block = $layout->createBlock('Training\Test\Block\Test');
-        $this->getResponse()->appendBody($block->_toHtml());
+//        $layoutFactory = $this->layoutFactory->create();
+//        $block = $layoutFactory->createBlock('Training\Test\Block\Test');
+//        $this->getResponse()->appendBody($block->_toHtml());
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('');
+        return $resultRaw;
     }
 }
