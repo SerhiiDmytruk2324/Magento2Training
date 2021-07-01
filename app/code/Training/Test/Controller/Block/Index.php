8d27d6fcc966eeedf7ce9134211b3c5382a4836c
Serhii Dmytruk <dmytruk.serhii@gmail.com>
2021-07-01 03:09:36 +0300

    3.2 done

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -2,13 +2,18 @@
 
 namespace Training\Test\Controller\Block;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+
+class Index extends \Magento\Framework\App\Action\Action implements HttpGetActionInterface, HttpPostActionInterface
 {
     /**
      * @var \Magento\Framework\View\LayoutFactory
      */
     private $layoutFactory;
+
     /**
+     * Index constructor.
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
      */
@@ -17,11 +22,12 @@ class Index extends \Magento\Framework\App\Action\Action
         \Magento\Framework\View\LayoutFactory $layoutFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        parent::__construct($context);
     }
     public function execute()
     {
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock('Training\Test\Block\Test');
-        $this->getResponse()->appendBody($block->toHtml());
+        $this->getResponse()->appendBody($block->_toHtml());
     }
 }
